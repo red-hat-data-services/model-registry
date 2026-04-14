6a9edff3ffd5a66a6be899c7d72024eecb50cdef
Dan Kuc <dkuc@redhat.com>
2026-04-14 09:10:59 -0500

    Small PR feedback changes

--- jobs/async-upload/local_hermetic_build.sh
+++ jobs/async-upload/local_hermetic_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Builds the async upload job hermetically using podman. The hermeto CLI expects to be ran from the git root
-set -ex
+set -euxo pipefail
 shopt -s expand_aliases
 mkdir -p /tmp/cachi2
-alias hermeto='podman run --rm -ti -v "$PWD:$PWD:z" -w "$PWD" -v /tmp/cachi2:/tmp/cachi2 ghcr.io/hermetoproject/hermeto:latest'
+alias hermeto='podman run --rm -ti -v "$PWD:$PWD:z" -w "$PWD" -v /tmp/cachi2:/tmp/cachi2:z ghcr.io/hermetoproject/hermeto:latest'
 
 hermeto fetch-deps --output /tmp/cachi2/output '[{"type": "rpm", "path": "jobs/async-upload"},{
   "type": "pip", "path": "jobs/async-upload",
@@ -16,12 +16,13 @@ hermeto inject-files /tmp/cachi2/output --for-output-dir /cachi2/output
 hermeto generate-env -f env -o /tmp/cachi2/cachi2.env --for-output-dir /tmp/cachi2 /tmp/cachi2/output
 
 podman build -f jobs/async-upload/Dockerfile.konflux jobs/async-upload/ \
-  --volume /tmp/cachi2:/cachi2:Z \
-  --volume /tmp/cachi2/output/deps/rpm/x86_64/repos.d:/etc/yum.repos.d \
+  --volume /tmp/cachi2:/cachi2:z \
+  --volume /tmp/cachi2/output/deps/rpm/x86_64/repos.d:/etc/yum.repos.d:z \
   --network none \
   --env PIP_NO_BINARY=:all: \
   --env CARGO_HOME=/cachi2/output/.cargo \
   --env PIP_FIND_LINKS=/cachi2/output/deps/pip \
   --env PIP_NO_INDEX=true \
   --build-arg TARGETARCH=x86_64 \
+  --arch x86_64 \
   --tag async-job
