7d786ea0221a4e5e7ee326fcfec36c5b73dd6aa8
Dan Kuc <dkuc@redhat.com>
2026-06-25 15:07:07 -0500

    use aipcc base image and binary wheels
    Signed-off-by: Dan Kuc <dkuc@redhat.com>

--- jobs/async-upload/local_hermetic_build.sh
+++ jobs/async-upload/local_hermetic_build.sh
@@ -5,21 +5,20 @@ shopt -s expand_aliases
 mkdir -p /tmp/cachi2
 alias hermeto='podman run --rm -ti -v "$PWD:$PWD:z" -w "$PWD" -v /tmp/cachi2:/tmp/cachi2:z ghcr.io/hermetoproject/hermeto:latest'
 
+
 hermeto fetch-deps --output /tmp/cachi2/output '[{"type": "rpm", "path": "jobs/async-upload"},{
   "type": "pip", "path": "jobs/async-upload",
-  "requirements_files": ["requirements.txt"],
-  "requirements_build_files": ["requirements-build.txt","requirements-extra-build-deps.txt"],
+  "requirements_files": ["requirements-aipcc.txt"],
   "binary": { "arch": "x86_64", "os": "linux" }
 }]'
 
 hermeto inject-files /tmp/cachi2/output --for-output-dir /cachi2/output
 hermeto generate-env -f env -o /tmp/cachi2/cachi2.env --for-output-dir /tmp/cachi2 /tmp/cachi2/output
 
-podman build -f jobs/async-upload/Dockerfile.konflux jobs/async-upload/ \
+podman build --no-cache -f jobs/async-upload/Dockerfile.konflux jobs/async-upload/ \
   --volume /tmp/cachi2:/cachi2:z \
   --volume /tmp/cachi2/output/deps/rpm/x86_64/repos.d:/etc/yum.repos.d:z \
   --network none \
-  --env PIP_NO_BINARY=:all: \
   --env CARGO_HOME=/cachi2/output/.cargo \
   --env PIP_FIND_LINKS=/cachi2/output/deps/pip \
   --env PIP_NO_INDEX=true \
