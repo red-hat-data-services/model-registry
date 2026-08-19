3bb8cf27365ffbeac8936e18ccd3d486d1f38086
Pushpa Padti <99261071+ppadti@users.noreply.github.com>
2026-08-19 17:09:49 +0530

    Add scroll inside serverJson card in MCP details page (#3099)
    * Make serverJson scrollable inside card in MCP details page
    
    Signed-off-by: ppadti <ppadti@redhat.com>
    
    * Make the clipboard copy sticky for serverJson
    
    Signed-off-by: ppadti <ppadti@redhat.com>
    
    * Fix cypress test
    
    Signed-off-by: ppadti <ppadti@redhat.com>
    
    ---------
    
    Signed-off-by: ppadti <ppadti@redhat.com>

--- clients/ui/frontend/src/__tests__/cypress/cypress/pages/mcpCatalog.ts
+++ clients/ui/frontend/src/__tests__/cypress/cypress/pages/mcpCatalog.ts
@@ -126,6 +126,10 @@ class McpServerDetails {
     return cy.findByTestId('mcp-server-json-code');
   }
 
+  findServerJsonScroll() {
+    return cy.findByTestId('mcp-server-json-scroll');
+  }
+
   findVersion() {
     return cy.findByTestId('mcp-server-version');
   }
